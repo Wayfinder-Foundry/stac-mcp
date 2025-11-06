de563d87b86c9733e7402002e4da061e96b3c3f3
GitHub Action <action@github.com>
2025-11-06 21:37:24 +0000

    chore: bump version to 2.1.2 (auto-increment from fix/item-search branch)

--- stac_mcp/__init__.py
+++ stac_mcp/__init__.py
@@ -4,7 +4,7 @@ This package exposes the server version and selected observability helpers
 for tests (see ADR 0012).
 """
 
-__version__ = "2.1.1"
+__version__ = "2.1.2"
 
 from .observability import metrics_latency_snapshot, metrics_snapshot
 
